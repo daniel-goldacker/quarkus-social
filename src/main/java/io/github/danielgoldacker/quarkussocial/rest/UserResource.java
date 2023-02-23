7d2287272a712e41b94ec35bb8cd5035ad7eed69
Daniel Goldacker <daniel-goldacker@hotmail.com>
2023-02-23 08:12:35 -0300

    Recebendo/Devolvendo  objetos JSON

--- src/main/java/io/github/danielgoldacker/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/danielgoldacker/quarkussocial/rest/UserResource.java
@@ -1,16 +1,29 @@
 package io.github.danielgoldacker.quarkussocial.rest;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import io.github.danielgoldacker.quarkussocial.rest.dto.CreateUserRequest;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
+
     @POST
     public Response createUsers(CreateUserRequest userRequest){
-        return Response.ok().build();
+        return Response.ok(userRequest).build();
 
     }
+
+    @GET
+    public Response listaAllUsers(){
+        return Response.ok().build();
+    }
+
 }
