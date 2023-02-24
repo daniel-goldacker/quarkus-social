7438e808947f652b314bdbfd34a62ae588aed2df
Daniel Goldacker <daniel-goldacker@hotmail.com>
2023-02-24 15:11:25 -0300

    Adicionando códigos http status mais específicos

--- src/main/java/io/github/danielgoldacker/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/danielgoldacker/quarkussocial/rest/UserResource.java
@@ -36,8 +36,7 @@ public class UserResource {
         
         Set<ConstraintViolation<CreateUserRequest>> violations = validator.validate(userRequest);
         if (!violations.isEmpty()){
-            ResponseError responseError = ResponseError.createFromValidation(violations);
-            return Response.status(400).entity(responseError).build();
+            return ResponseError.createFromValidation(violations).withStatusCode(ResponseError.UNPROCESSABLE_ENTITY_STATUS);
         }
 
         User user = new User();
@@ -46,7 +45,7 @@ public class UserResource {
 
         repository.persist(user);
 
-        return Response.ok(user).build();
+        return Response.status(Response.Status.CREATED.getStatusCode()).entity(user).build();
     }
 
     @GET
@@ -64,7 +63,7 @@ public class UserResource {
 
         if (user != null) {
             repository.delete(user);
-            return Response.ok().build();
+            return Response.noContent().build();
         } else {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
--- src/main/java/io/github/danielgoldacker/quarkussocial/rest/dto/ResponseError.java
+++ src/main/java/io/github/danielgoldacker/quarkussocial/rest/dto/ResponseError.java
@@ -6,9 +6,10 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.validation.ConstraintViolation;
+import javax.ws.rs.core.Response;
 
 public class ResponseError {
- 
+    public static final int UNPROCESSABLE_ENTITY_STATUS = 422;
     private String message;
     private Collection<FieldError> errors;
 
@@ -25,6 +26,10 @@ public class ResponseError {
         var responseError = new ResponseError(message, errors);
         return responseError;
     }
+
+    public Response withStatusCode(int code){
+        return Response.status(code).entity(this).build();   
+     }
 
     public String getMessage() {
         return this.message;
