223162598c2ff21dc3f21b2785cff77de9fe719c
Daniel Goldacker <daniel-goldacker@hotmail.com>
2023-02-28 08:20:17 -0300

    implementado método PUT na api de followers

--- src/main/java/io/github/danielgoldacker/quarkussocial/domain/repository/FollowerRepository.java
+++ src/main/java/io/github/danielgoldacker/quarkussocial/domain/repository/FollowerRepository.java
@@ -1,11 +1,27 @@
 package io.github.danielgoldacker.quarkussocial.domain.repository;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 import javax.enterprise.context.ApplicationScoped;
 
 import io.github.danielgoldacker.quarkussocial.domain.model.Follower;
+import io.github.danielgoldacker.quarkussocial.domain.model.User;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 @ApplicationScoped
 public class FollowerRepository implements PanacheRepository<Follower> {
-    
+    public boolean follows(User follower, User user) {
+
+        Map<String, Object> param = new HashMap<>();
+        param.put("follower", follower);
+        param.put("user", user);
+
+        PanacheQuery<Follower> query = find("follower  = :follower and user = :user", param);
+        Optional<Follower> result = query.firstResultOptional();
+        
+        return result.isPresent();
+    }
 }
--- src/main/java/io/github/danielgoldacker/quarkussocial/rest/FollowerResource.java
+++ src/main/java/io/github/danielgoldacker/quarkussocial/rest/FollowerResource.java
@@ -1,13 +1,20 @@
 package io.github.danielgoldacker.quarkussocial.rest;
 
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import io.github.danielgoldacker.quarkussocial.domain.model.Follower;
+import io.github.danielgoldacker.quarkussocial.domain.model.User;
+import io.github.danielgoldacker.quarkussocial.domain.repository.FollowerRepository;
 import io.github.danielgoldacker.quarkussocial.domain.repository.UserRepository;
+import io.github.danielgoldacker.quarkussocial.rest.dto.FollowerRequest;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 @Path("/users/{userId}/followers")
@@ -16,13 +23,38 @@ import javax.ws.rs.Produces;
 public class FollowerResource {
 
     private UserRepository userRepository;
-    private Follower repository;
+    private FollowerRepository repository;
 
     @Inject
-    public FollowerResource(Follower repository, UserRepository userRepository) {
+    public FollowerResource(FollowerRepository repository, UserRepository userRepository) {
         this.repository = repository;
         this.userRepository = userRepository;
     }
     
+    @PUT
+    @Transactional
+    public Response followUser(@PathParam("userId") Long userId, FollowerRequest request){
+      User user = userRepository.findById(userId);
+
+      if (user == null) {
+          return Response.status(Response.Status.NOT_FOUND).build();
+      }
+
+      User follower = userRepository.findById(request.getFollowerId());      
+      if (follower == null) {
+        return Response.status(Response.Status.NOT_FOUND).build();
+      }
+
+      boolean follows = repository.follows(follower, user);
+      if (!follows){
+        Follower entity = new Follower(); 
+        entity.setUser(user);
+        entity.setFollower(follower);
+  
+        repository.persist(entity);
+      }
+
+      return Response.status(Response.Status.NO_CONTENT).build();
+    }
 
 }
