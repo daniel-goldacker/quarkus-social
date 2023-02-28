f4103840019cce74bd3dbcbc0b90c97045ee4a43
Daniel Goldacker <daniel-goldacker@hotmail.com>
2023-02-28 08:34:52 -0300

    removido import desnecessários

--- src/main/java/io/github/danielgoldacker/quarkussocial/domain/model/Follower.java
+++ src/main/java/io/github/danielgoldacker/quarkussocial/domain/model/Follower.java
@@ -1,6 +1,5 @@
 package io.github.danielgoldacker.quarkussocial.domain.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
--- src/main/java/io/github/danielgoldacker/quarkussocial/rest/FollowerResource.java
+++ src/main/java/io/github/danielgoldacker/quarkussocial/rest/FollowerResource.java
@@ -1,9 +1,5 @@
 package io.github.danielgoldacker.quarkussocial.rest;
 
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Request;
-import javax.ws.rs.core.Response;
-
 import io.github.danielgoldacker.quarkussocial.domain.model.Follower;
 import io.github.danielgoldacker.quarkussocial.domain.model.User;
 import io.github.danielgoldacker.quarkussocial.domain.repository.FollowerRepository;
@@ -17,6 +13,9 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 
 @Path("/users/{userId}/followers")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -61,5 +60,4 @@ public class FollowerResource {
 
       return Response.status(Response.Status.NO_CONTENT).build();
     }
-
 }
